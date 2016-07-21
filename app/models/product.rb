1e25fbb2d668d223607c12ad95b1bc70347c69c2
mikeSheets <mike.sheets@wantable.com>
2016-07-21 16:30:14 -0500

    Angular picture stuff --mjs--

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ActiveRecord::Base
   before_validation :set_permalink
 
   def formatted_image
-    "assets/products/" + image.split("/").last
+    ActionController::Base.helpers.image_url("assets/products/" + image.split("/").last)
   end
 
   def formatted_link

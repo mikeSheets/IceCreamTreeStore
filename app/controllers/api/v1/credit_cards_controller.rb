07c662d4f3be1be59ebcbe210d0feba8f42b5081
mikeSheets <mike.sheets@wantable.com>
2016-07-21 12:45:54 -0500

    Playing with heroku --mjs--

--- app/controllers/api/v1/credit_cards_controller.rb
+++ app/controllers/api/v1/credit_cards_controller.rb
@@ -1,28 +1,27 @@
 class Api::V1::CreditCardsController < ApplicationController
-  load_and_authorize_resource CreditCard
+  load_and_authorize_resource
 
   def create
-    cc = CreditCard.new(cc_params)
-    cc.user_id = current_user.id
+    @credit_card.user_id = current_user.id
 
-    if cc.save
-      render json: cc.to_json
+    if @credit_card.save
+      render json: @credit_card.to_json
     else
-      render json: cc.errors.to_json, status: 400
+      render json: @credit_card.errors.to_json, status: 400
     end
   end
 
   def update
-    cc = CreditCard.find(params[:id])
-    if cc.update(cc_params)
-      render json: cc.to_json
+    if @credit_card.update(credit_card_params)
+      render json: @credit_card.to_json
     else
-      render json: cc.errors.to_json, status: 400
+      render json: @credit_card.errors.to_json, status: 400
     end
   end
 
   protected
-  def cc_params
+
+  def credit_card_params
     params.require(:credit_card).permit(:last_four, :month, :year, :user_id, :name, :number, :cvc)
   end
 end
